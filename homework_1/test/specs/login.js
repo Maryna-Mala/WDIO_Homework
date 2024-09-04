cbbefaadd2961007c254ec18705b97e5a61b558f
Maryna Mala <marinamalaa0@gmail.com>
2024-09-04 17:29:20 +0300

    login.js updated

--- homework_1/test/specs/login.js
+++ homework_1/test/specs/login.js
@@ -6,17 +6,17 @@ describe('Trello log in', () => {
     beforeEach(async () => {
         await homePage.open()
         await homePage.header.logInButton()
-        await homePage.logInModal.logInInput(LOGIN_EMAIL)
+        await homePage.logInModal.logInInput(process.env.LOGIN_EMAIL)
         await homePage.logInModal.submitButton()
     })
 
     it('Login with valid credentials for free account', async () => {
-        await homePage.logInModal.validPassword(VALID_PASSWORD)
+        await homePage.logInModal.validPassword(process.env.VALID_PASSWORD)
         await homePage.logInModal.submitButton()
     })
 
     it('Login with  existent account and invalid password', async () => {
-        await homePage.logInModal.invalidPassword(INVALID_PASSWORD)
+        await homePage.logInModal.invalidPassword(process.env.INVALID_PASSWORD)
         await homePage.logInModal.submitButton()
         expect(await homePage.logInModal.passwordInput.isDisplayed()).to.be.true;
     })
